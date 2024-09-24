db8ab7454943deccadf6f9444466f4d659f74514
EzequiasBarreto <ezequias.barreto@yahoo.com.br>
2024-09-24 12:56:19 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Postagem } from './postagem/postagem.entity/postagem.entity';
 import { Membro } from './membro/membro.entity/membro.entity';
+/*
 
+
+
+
+*/
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',   // Aqui estamos dizendo que o banco será MySQL
-      host: 'localhost',  // Ou o IP do seu servidor MySQL
-      port: 3306,  // Porta padrão do MySQL
-      username: 'root',  // Usuário do MySQL (ajuste se necessário)
-      password: '110464',  // Senha do MySQL
-      database: 'projeto_teste',  // Nome do banco de dados que criamos
-      entities: [Postagem, Membro],  // Nossas entidades
-      synchronize: true,  // Atualiza o banco com base nas entidades (cuidado em produção)
+      type: 'mysql',   
+      host: 'localhost',  
+      port: 3306, 
+      username: 'root',  
+      password: '110464',  
+      database: 'projeto_teste',
+      entities: [Postagem, Membro],  
+      synchronize: true,  
     }),
-    TypeOrmModule.forFeature([Postagem, Membro]),  // Registra as entidades que vamos usar
+    TypeOrmModule.forFeature([Postagem, Membro]),  
   ],
 })
 export class AppModule {}
